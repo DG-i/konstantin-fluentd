65aea55ff0ac3065af8cc1b5e38130e9fe82d8e1
Andreas Dembach <ad@dg-i.net>
2016-02-19 23:55:49 +0100

    Update tdagent.rb
    copied features from the inherited gem provider

--- lib/puppet/provider/package/tdagent.rb
+++ lib/puppet/provider/package/tdagent.rb
@@ -1,6 +1,7 @@
 # This file must be compatible with Ruby 1.8.7 in order to work on EL6.
 module Puppet::Parser::Functions
   Puppet::Type.type(:package).provide :tdagent, :parent => :gem, :source => :gem do
+    has_feature :versionable, :install_options, :uninstall_options
     commands :gemcmd => '/opt/td-agent/usr/sbin/td-agent-gem'
   end
 end
